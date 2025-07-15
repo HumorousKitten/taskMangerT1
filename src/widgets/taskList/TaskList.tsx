93735b4f12ac23cbb282592574d6172fd15bd16f
vladiks <vladvannikov@gmail.com>
2025-07-15 15:57:11 +0400

    feat: add prettier and eslint

--- src/widgets/taskList/TaskList.tsx
+++ src/widgets/taskList/TaskList.tsx
@@ -1,44 +1,41 @@
-import React from 'react'
-import { FC } from 'react'
+import React, { FC } from 'react';
 
-import { Grid } from '@mui/material'
+import { Grid } from '@mui/material';
 
-import { ITask } from '@/shared/types/types'
+import { ITask } from '@/shared/types/types';
 
-import { AddTask } from '@/features/addTask/AddTask'
-import { TaskItem } from '@/features/taskItem/TaskItem'
+import { AddTask } from '@/features/addTask/AddTask';
+import { TaskItem } from '@/features/taskItem/TaskItem';
 
-import { useTaskStore } from '@/store/useTasksStore'
-
-
+import { useTaskStore } from '@/store/useTasksStore';
 
 interface ITaskListProps {
-	filters: Pick<ITask, 'status' | 'category' | 'priority'>
+  filters: Pick<ITask, 'status' | 'category' | 'priority'>;
 }
 
 export const TaskList: FC<ITaskListProps> = ({ filters }) => {
-	const tasks = useTaskStore(state => state.tasks)
-	const filteredTasks = React.useMemo(() => {
-		return tasks.filter(task => {
-			return (
-				(filters.category === '' || task.category === filters.category) &&
-				(filters.priority === '' || task.priority === filters.priority) &&
-				(filters.status === '' || task.status === filters.status)
-			)
-		})
-	}, [tasks, filters])
+  const tasks = useTaskStore((state) => state.tasks);
+  const filteredTasks = React.useMemo(() => {
+    return tasks.filter((task) => {
+      return (
+        (filters.category === '' || task.category === filters.category) &&
+        (filters.priority === '' || task.priority === filters.priority) &&
+        (filters.status === '' || task.status === filters.status)
+      );
+    });
+  }, [tasks, filters]);
 
-	return (
-		<Grid container spacing={2} justifyContent='center'>
-			{filteredTasks.map(item => (
-				<Grid key={item.id}>
-					<TaskItem task={item} />
-				</Grid>
-			))}
+  return (
+    <Grid container spacing={2} justifyContent="center">
+      {filteredTasks.map((item) => (
+        <Grid key={item.id}>
+          <TaskItem task={item} />
+        </Grid>
+      ))}
 
-			<Grid>
-				<AddTask />
-			</Grid>
-		</Grid>
-	)
-}
+      <Grid>
+        <AddTask />
+      </Grid>
+    </Grid>
+  );
+};
